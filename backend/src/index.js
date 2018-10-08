4bb9fa831af6c01100a1ab331a5754bb6e97d89c
Olanrewaju Oladunjoye <olanrewajusoladunjoye@gmail.com>
2018-10-08 22:46:22 +0300

    fix to move env variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
 
-const appId = process.env.APPID || 'e806a08a459993c79b06b152b06f2302';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
+const appId = process.env.APPID || ' ';
+const mapURI = process.env.MAP_ENDPOINT || " ";
 
-var targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+var targetCity = process.env.TARGET_CITY || " ";
 
 
 const port = process.env.PORT || 9000;
